45b6b52f322de70557e63f771dd565f603ad1853
Dario <dario.ferrai@factorymind.com>
2019-10-29 17:07:24 +0100

    Moving Images proj to NetFramework to test GPU

--- examples/ReinforcementLearning/ReinforcementLearning.Images.Runner/BreakoutConfiguration.cs
+++ examples/ReinforcementLearning/ReinforcementLearning.Images.Runner/BreakoutConfiguration.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Gym.Environments.Envs.Atari;
 using Gym.Envs;
-using Gym.Rendering.Avalonia;
+using Gym.Rendering.WinForm.Rendering;
 using NeuralNetworkNET.APIs;
 using NeuralNetworkNET.APIs.Enums;
 using NeuralNetworkNET.APIs.Interfaces;
@@ -11,7 +11,7 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace ReinforcementLearning.Images.Runner {
     public sealed class BreakoutConfiguration : IImageGameConfiguration {
-        private readonly Lazy<IEnv> _env = new Lazy<IEnv>(() => new BreakoutEnv(AvaloniaEnvViewer.Run));
+        private readonly Lazy<IEnv> _env = new Lazy<IEnv>(() => new BreakoutEnv(WinFormViewer.Run));
 
         public IEnv EnvInstance => _env.Value;
         public int MemoryStates => 2;
--- examples/ReinforcementLearning/ReinforcementLearning.Images.Runner/CartPoleConfiguration.cs
+++ examples/ReinforcementLearning/ReinforcementLearning.Images.Runner/CartPoleConfiguration.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Gym.Environments.Envs.Classic;
 using Gym.Envs;
-using Gym.Rendering.Avalonia;
+using Gym.Rendering.WinForm.Rendering;
 using NeuralNetworkNET.APIs;
 using NeuralNetworkNET.APIs.Enums;
 using NeuralNetworkNET.APIs.Interfaces;
@@ -11,7 +11,7 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace ReinforcementLearning.Images.Runner {
     public sealed class CartPoleConfiguration : IImageGameConfiguration {
-        private readonly Lazy<IEnv> _env = new Lazy<IEnv>(() => new CartPoleEnv(AvaloniaEnvViewer.Run));
+        private readonly Lazy<IEnv> _env = new Lazy<IEnv>(() => new CartPoleEnv(WinFormViewer.Run));
 
         public IEnv EnvInstance => _env.Value;
         public int MemoryStates => 2;
